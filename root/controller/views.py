6321564240681a936f6b1ab2d1747fd902b80f00
Alex <hyoungseok.K@gmail.com>
2016-12-23 18:57:01 +0900

    main code commit

---
+++ root/controller/views.py
@@ -0,0 +1,15 @@
+# -*- coding: utf-8 -*-
+
+from root import app
+from flask import render_template, request
+
+
+@app.route('/')
+def index():
+    return render_template('bingo.html')
+
+
+@app.route('/play', methods=['GET', 'POST'])
+def start():
+    input_code = request.args.get('code')
+    return render_template('play.html', input_code=input_code)
